d8e3f8e14d94fd8641b2690e35917fe004181218
ArtemSudakov <siteartstudio@gmail.com>
2018-01-18 15:22:01 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+include "inc/header.php"; 
+include "inc/dbconnect.php";
+?>
+
+<script src="js/load_news.js" async=""></script>      
+<script src="js/serch.js" async=""></script>
+<script src="js/add_coment.js" async=""></script>
+        
+<?php include "inc/futer.php"; ?>
